f207553b787965ab13a27564f79ddb08eefb0b3b
Martin Kubat <martinkubat@icloud.com>
2016-11-19 12:58:40 +0000

    Made empty class

---
+++ Novis Obliviate/Novis Obliviate/MicrophoneProcessor.swift
@@ -0,0 +1,16 @@
+//
+//  MicrophoneProcessor.swift
+//  Novis Obliviate
+//
+//  Created by Martin Hartt on 19/11/2016.
+//  Copyright © 2016 Novis Obliviate Ltd. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class MicrophoneProcessor: NSObject {
+  
+  
+  
+}
