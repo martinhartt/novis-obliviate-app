f5cfdb0a9c31f9746ce673821e47f72d9bec65c4
Martin Kubat <martinkubat@icloud.com>
2016-11-19 12:43:12 +0000

    Created project

---
+++ Novis Obliviate/Novis Obliviate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Novis Obliviate
+//
+//  Created by Martin Kubat on 19/11/2016.
+//  Copyright © 2016 Novis Obliviate Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
