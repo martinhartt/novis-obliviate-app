5c464d391011353000a46ef30dacbb83c224dba2
Martin Kubat <martinkubat@icloud.com>
2016-11-19 13:16:13 +0000

    Added some basic functionality

--- Novis Obliviate/Novis Obliviate/MicrophoneProcessor.swift
+++ Novis Obliviate/Novis Obliviate/MicrophoneProcessor.swift
@@ -11,6 +11,63 @@ import AVFoundation
 
 class MicrophoneProcessor: NSObject {
   
+  var audioRecorder: AVAudioRecorder?
+  
+  override init() {
+    super.init()
+    
+    
+    let directories = FileManager.default.urls(for: .documentDirectory,
+                                            in: .userDomainMask)
+    
+    let soundFileURL = directories[0].appendingPathComponent("sound.caf")
+
+    
+    let recordSettings =
+      [AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
+       AVEncoderBitRateKey: 16,
+       AVNumberOfChannelsKey: 2,
+       AVSampleRateKey: 44100.0] as [String : Any]
+    
+    let audioSession = AVAudioSession.sharedInstance()
+    
+    do {
+      try audioSession.setCategory(
+        AVAudioSessionCategoryPlayAndRecord)
+    } catch let error as NSError {
+      print("audioSession error: \(error.localizedDescription)")
+    }
+    
+    do {
+      self.audioRecorder = try? AVAudioRecorder(url: soundFileURL, settings: recordSettings as [String : AnyObject])
+      audioRecorder?.prepareToRecord()
+      audioRecorder?.isMeteringEnabled = true
+      
+    } catch let error as NSError {
+      print("audioSession error: \(error.localizedDescription)")
+    }
+    
+    startRecording()
+  }
+  
+  func startRecording() {
+    audioRecorder?.record()
+    let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(MicrophoneProcessor.test), userInfo: nil, repeats: true)
+    print("Start recording")
+  }
+  
+  func test() {
+    print("Hello!")
+    
+    audioRecorder?.updateMeters()
+    print(self.audioRecorder?.averagePower(forChannel: 0))
+  }
+  
+  
+  func stopRecording() {
+    audioRecorder?.stop()
+  }
+  
   
   
 }
